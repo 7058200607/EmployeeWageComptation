f1e96f5643daaa5c4fae8a480668a0b1b7ee05f7
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-07-14 15:42:34 +0530

    Calculate Employee Wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,17 +8,22 @@ namespace EmpWageComputation
         {
 
             int Is_full_time = 1;
+            int Emp_Rate_per_Hour=20;
+            int Emp_Hour= 0;
+            int Emp_Wage=0;
             Random random = new Random();
             int EmpCheck = random.Next(0, 2);
             if (Is_full_time == EmpCheck) 
             {
-                Console.WriteLine("Employee Is Present");
+                Emp_Hour = 8;
 
             }
             else
             {
-                Console.WriteLine("Employee Is Absent");
+                Emp_Hour = 0;
             }
+            Emp_Wage = Emp_Rate_per_Hour * Emp_Hour;
+            Console.WriteLine("Emp_Wage:"+Emp_Wage);
             
         }
     }
