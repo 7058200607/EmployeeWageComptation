6c85f5e0d7bbcf10599b16f9d3e4ea07b72ecc31
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-07-14 15:01:24 +0530

    welcome to Employee Wage Computation

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmpWageComputation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+        }
+    }
+}
+
+
