1817fc1a36cc0d5c4c277a68982548ba0b744922
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-07-14 15:25:46 +0530

    employee is present or not

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,7 +6,20 @@ namespace EmpWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation");
+
+            int Is_full_time = 1;
+            Random random = new Random();
+            int EmpCheck = random.Next(0, 2);
+            if (Is_full_time == EmpCheck) 
+            {
+                Console.WriteLine("Employee Is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+            }
+            
         }
     }
 }
